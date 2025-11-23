269e55a310938ccc1a4eac5b8853062aff0dfe6c
Yannik Heizmann <github@yannikheizmann.com>
2025-11-23 10:49:24 +0100

    fixed matplotlib backend for colab

--- src/trackerclassification/dataset/sample/_visualize.py
+++ src/trackerclassification/dataset/sample/_visualize.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 import numpy as np
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import math
